539fe232fb96500d23dd7ed0c93b452c6e370310
gcalvo506 <39446138+gcalvo506@users.noreply.github.com>
2018-06-09 16:15:08 -0600

    Archivo Contactos
    Clase de los objetos contactos, gets & setters

---
+++ Contactos.java
@@ -0,0 +1,207 @@
+package com.company;
+
+public class Contactos {
+    String Nombre_de_Contacto;
+    Boolean ind_imagen;
+    int telefonoMovil,telefonoTrabajo,telefonoCasa,telefonoFax,telefonoOtro;
+    String correo1,correo2,correo3;
+    String sonidoLlamada,sonidoMsj,sonidoCorreo,sonidoSilencio;
+    String direccionCasa,direccionTrabajo;
+    String lugarTrabajo,lugarEstudio;
+    String notas;
+    String familia;
+    String Profesion;
+    String FechaCumple,FechaMatrimonio,FechaTrabajo;
+
+    public String getNombre_de_Contacto() {
+        return Nombre_de_Contacto;
+    }
+
+    public void setNombre_de_Contacto(String nombre_de_Contacto) {
+        Nombre_de_Contacto = nombre_de_Contacto;
+    }
+
+    public Boolean getInd_imagen() {
+        return ind_imagen;
+    }
+
+    public void setInd_imagen(Boolean ind_imagen) {
+        this.ind_imagen = ind_imagen;
+    }
+
+    public int getTelefonoMovil() {
+        return telefonoMovil;
+    }
+
+    public void setTelefonoMovil(int telefonoMovil) {
+        this.telefonoMovil = telefonoMovil;
+    }
+
+    public int getTelefonoTrabajo() {
+        return telefonoTrabajo;
+    }
+
+    public void setTelefonoTrabajo(int telefonoTrabajo) {
+        this.telefonoTrabajo = telefonoTrabajo;
+    }
+
+    public int getTelefonoCasa() {
+        return telefonoCasa;
+    }
+
+    public void setTelefonoCasa(int telefonoCasa) {
+        this.telefonoCasa = telefonoCasa;
+    }
+
+    public int getTelefonoFax() {
+        return telefonoFax;
+    }
+
+    public void setTelefonoFax(int telefonoFax) {
+        this.telefonoFax = telefonoFax;
+    }
+
+    public int getTelefonoOtro() {
+        return telefonoOtro;
+    }
+
+    public void setTelefonoOtro(int telefonoOtro) {
+        this.telefonoOtro = telefonoOtro;
+    }
+
+    public String getCorreo1() {
+        return correo1;
+    }
+
+    public void setCorreo1(String correo1) {
+        this.correo1 = correo1;
+    }
+
+    public String getCorreo2() {
+        return correo2;
+    }
+
+    public void setCorreo2(String correo2) {
+        this.correo2 = correo2;
+    }
+
+    public String getCorreo3() {
+        return correo3;
+    }
+
+    public void setCorreo3(String correo3) {
+        this.correo3 = correo3;
+    }
+
+    public String getSonidoLlamada() {
+        return sonidoLlamada;
+    }
+
+    public void setSonidoLlamada(String sonidoLlamada) {
+        this.sonidoLlamada = sonidoLlamada;
+    }
+
+    public String getSonidoMsj() {
+        return sonidoMsj;
+    }
+
+    public void setSonidoMsj(String sonidoMsj) {
+        this.sonidoMsj = sonidoMsj;
+    }
+
+    public String getSonidoCorreo() {
+        return sonidoCorreo;
+    }
+
+    public void setSonidoCorreo(String sonidoCorreo) {
+        this.sonidoCorreo = sonidoCorreo;
+    }
+
+    public String getSonidoSilencio() {
+        return sonidoSilencio;
+    }
+
+    public void setSonidoSilencio(String sonidoSilencio) {
+        this.sonidoSilencio = sonidoSilencio;
+    }
+
+    public String getDireccionCasa() {
+        return direccionCasa;
+    }
+
+    public void setDireccionCasa(String direccionCasa) {
+        this.direccionCasa = direccionCasa;
+    }
+
+    public String getDireccionTrabajo() {
+        return direccionTrabajo;
+    }
+
+    public void setDireccionTrabajo(String direccionTrabajo) {
+        this.direccionTrabajo = direccionTrabajo;
+    }
+
+    public String getLugarTrabajo() {
+        return lugarTrabajo;
+    }
+
+    public void setLugarTrabajo(String lugarTrabajo) {
+        this.lugarTrabajo = lugarTrabajo;
+    }
+
+    public String getLugarEstudio() {
+        return lugarEstudio;
+    }
+
+    public void setLugarEstudio(String lugarEstudio) {
+        this.lugarEstudio = lugarEstudio;
+    }
+
+    public String getNotas() {
+        return notas;
+    }
+
+    public void setNotas(String notas) {
+        this.notas = notas;
+    }
+
+    public String getFamilia() {
+        return familia;
+    }
+
+    public void setFamilia(String familia) {
+        this.familia = familia;
+    }
+
+    public String getProfesion() {
+        return Profesion;
+    }
+
+    public void setProfesion(String profesion) {
+        Profesion = profesion;
+    }
+
+    public String getFechaCumple() {
+        return FechaCumple;
+    }
+
+    public void setFechaCumple(String fechaCumple) {
+        FechaCumple = fechaCumple;
+    }
+
+    public String getFechaMatrimonio() {
+        return FechaMatrimonio;
+    }
+
+    public void setFechaMatrimonio(String fechaMatrimonio) {
+        FechaMatrimonio = fechaMatrimonio;
+    }
+
+    public String getFechaTrabajo() {
+        return FechaTrabajo;
+    }
+
+    public void setFechaTrabajo(String fechaTrabajo) {
+        FechaTrabajo = fechaTrabajo;
+    }
+}
